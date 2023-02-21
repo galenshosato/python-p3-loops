6f57ee0b55eb06259392a825ae03ba8bc23be482
Galen Sho Sato <galen.sato@gmail.com>
2023-02-20 22:22:05 -0500

    Completed Lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    for i in range(10, 0, -1):
-        print(i)
-    
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
     print("Happy New Year!")
 
-def square_integers(int_list):
-    return [i ** 2 for i in int_list]
+def square_integers(array):
+    squared = [square * square for square in array]
+    return squared
+
+
 
 def fizzbuzz():
-    for i in range(1, 101):
-        if not i % 15:
+    
+    for i in range(1,101) :
+        if i % 3 == 0 and  i % 5 == 0:
             print("FizzBuzz")
-        elif not i % 5:
-            print("Buzz")
-        elif not i % 3:
+        elif i % 3 == 0:
             print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
         else:
-            print(i)
+            print(i) 
+
